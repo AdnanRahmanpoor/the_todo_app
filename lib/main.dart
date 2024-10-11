711be1673da5c2975557049c246c3d50a188c13f
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-12 00:18:35 +0400

    Added Swipe to delete function

--- lib/main.dart
+++ lib/main.dart
@@ -84,39 +84,76 @@ class TodoList extends StatelessWidget {
           itemCount: todoListProvider.todos.length,
           itemBuilder: (context, index) {
             final todo = todoListProvider.todos[index];
-            return Card(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              elevation: 4,
-              child: ListTile(
-                title: Text(
-                  todo.title,
-                  style: TextStyle(
-                    decoration:
-                        todo.completed ? TextDecoration.lineThrough : null,
-                    color: todo.completed ? Colors.grey : Colors.black,
+
+            return Dismissible(
+              key: Key(todo.id),
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(Icons.delete, color: Colors.white),
+              ),
+              direction: DismissDirection.endToStart,
+              confirmDismiss: (direction) async {
+                return await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Confirm Deletion'),
+                      content:
+                          Text('Are you sure you want to delete ${todo.title}'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text('Delete'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              onDismissed: (direction) {
+                Provider.of<TodoListProvider>(context, listen: false)
+                    .deleteTodo(todo.id);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Task Deleted')),
+                );
+              },
+              child: Card(
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                elevation: 4,
+                child: ListTile(
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(
+                      decoration:
+                          todo.completed ? TextDecoration.lineThrough : null,
+                      color: todo.completed ? Colors.grey : Colors.black,
+                    ),
+                  ),
+                  subtitle: todo.description != null
+                      ? Text(
+                          todo.description!,
+                          style: TextStyle(
+                            color: todo.completed
+                                ? Colors.grey
+                                : const Color.fromARGB(255, 96, 96, 96),
+                          ),
+                        )
+                      : null,
+                  trailing: Checkbox(
+                    value: todo.completed,
+                    onChanged: (value) {
+                      Provider.of<TodoListProvider>(context, listen: false)
+                          .toggleTodoCompletion(todo.id);
+                    },
                   ),
                 ),
-                subtitle: todo.description != null
-                    ? Text(
-                        todo.description!,
-                        style: TextStyle(
-                          color: todo.completed
-                              ? Colors.grey
-                              : const Color.fromARGB(255, 96, 96, 96),
-                        ),
-                      )
-                    : null,
-                trailing: Checkbox(
-                  value: todo.completed,
-                  onChanged: (value) {
-                    Provider.of<TodoListProvider>(context, listen: false)
-                        .toggleTodoCompletion(todo.id);
-                  },
-                ),
-                onLongPress: () {
-                  Provider.of<TodoListProvider>(context, listen: false)
-                      .deleteTodo(todo.id);
-                },
               ),
             );
           },
